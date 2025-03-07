c86dde636d87030796fcefab16d79b71c4bcc5ae
Mihir Chakma <mihirchakmaths@gmail.com>
2025-03-07 12:40:58 +0530

    update description of  hello world program

--- exercises/1.hello_world.py
+++ exercises/1.hello_world.py
@@ -1,3 +1,10 @@
+# EXERCISE #1: HELLO, WORLD! 
+
+# Print "Hello, world!" on the screen:
 print("Hello, World!")
+
+# Ask the user for their name, Get the user's name from their keyboard input: 
 name = input("What is your name? ")
+
+# Greet the user by their name: 
 print(f"Hello, {name}")
---
+++ exercises/2.temperature_conversion.py
@@ -0,0 +1,5 @@
+# EXERCISE #2: TEMPERATURE CONVERSION
+
+# convertToCelsius(32)  →  0.0 
+# convertToFahrenheit(100)  →  212 
+
